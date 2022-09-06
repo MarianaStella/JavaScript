be652a76b38c158338b95b822eb040a5a9e6458b
MarianaStella <marianasm125@gmail.com>
2022-09-06 12:02:05 -0300

    Agregué funciones

--- app.js
+++ app.js
@@ -1,28 +1,21 @@
-// EJEMPLO 1
+let zapatillas = 25000;
+let mochila = 14000;
 
-// let ingresarNumero = Number(prompt("Ingrese un número"));
-
-// for (let i = 1; i <= ingresarNumero; i++) {
-//     console.log("Bienvenido");
-// }
-
-
-// EJEMPLO 2
-
-
-// let ingresarNombre = prompt("Ingrese su nombre");
-
-// for (let i = 1; i <=30; i++){
-//     alert(`${ingresarNombre} su turno es el nº: ${+i}`);
-// }
-
-
-// EJEMPLO 3
-
-let nombre = prompt("Ingrese su nombre o ESC para salir");
-
-while(nombre != "ESC" && nombre != "esc"){
-    alert(`Hola ${nombre}, cómo dice que le va?`);
-    console.log("Hola" + nombre + ", cómo dice que le va?");
-    nombre = prompt("Ingrese otro nombre");
+function calculadora (zapatillas, mochila, operacion){
+    switch (operacion){
+        case "+":
+            return zapatillas + mochila;
+            break;
+        case "-":
+            return zapatillas - mochila;
+            break;
+        default:
+            return 0;
+            break;
+    }
 }
+
+console.log(calculadora(zapatillas, mochila, "+"));
+
+
+// Utilicé una función con el ciclo Switch para sumar o restar la compra de unas zapatillas y mochila. 
